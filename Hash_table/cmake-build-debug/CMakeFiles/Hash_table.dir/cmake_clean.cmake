6098b52b2e991aefe48ade8c3c55bbe92a7b4aa9
vlad <41704234+FallenCookies@users.noreply.github.com>
2019-08-07 20:33:46 +0400

    Hash_set Done

---
+++ Hash_table/cmake-build-debug/CMakeFiles/Hash_table.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hash_table.dir/main.cpp.obj"
+  "Hash_table.pdb"
+  "Hash_table.exe"
+  "Hash_table.exe.manifest"
+  "libHash_table.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hash_table.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
